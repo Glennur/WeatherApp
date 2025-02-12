1f5a60c79518f584ff1f6f4888f7be68c9eb0c2d
Glenn Wahlbeck <glenn.wahlbeck@campusnykoping.se>
2025-02-12 08:38:01 +0100

    added loop in program menu

--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -4,26 +4,31 @@
     {
         static void Main(string[] args)
         {
-            string fileName = "tempdata.txt";
-            Console.WriteLine("1. Medeltemp");
-            Console.WriteLine("2. Medelluftfuktighet");
-
-            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-            string date;
-            char key = keyInfo.KeyChar;
-           
-            switch(key)
+            while (true)
             {
-                case '1':
-                    date = Helper.Date("");
-                    
-                    AnalyzeNumbers.AverageTemp(date, fileName);
-                    
-                    break;
-                case '2':
-                    date = Helper.Date("");
-                    
-                    break;
+                Console.Clear();
+                string fileName = "tempdata.txt";
+                Console.WriteLine("1. Medeltemp");
+                Console.WriteLine("2. Medelluftfuktighet");
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                string date;
+                char key = keyInfo.KeyChar;
+
+                switch (key)
+                {
+                    case '1':
+                        date = Helper.Date("");
+
+                        AnalyzeNumbers.AverageTemp(date, fileName);
+                        Console.ReadKey();
+
+                        break;
+                    case '2':
+                        date = Helper.Date("");
+
+                        break;
+                }
             }
             
         }
